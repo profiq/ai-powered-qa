210a7f5a1fcdacf70348698cb748bb819f950bde
Viktor Nawrath <viktor.nawrath@profiq.com>
2023-10-13 11:52:34 +0200

    Small improvements for roundtable demo

--- web_ui.py
+++ web_ui.py
@@ -56,16 +56,14 @@ def load_conversation_history(project_name, test_case):
 
 @st.cache_resource
 def get_llm(project_name, test_case):
-
     async_browser = st.session_state.browser
-    toolkit = PlayWrightBrowserToolkit.from_browser(
-        async_browser=async_browser)
+    toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
     tools = toolkit.get_tools()
     llm = ChatOpenAI(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         streaming=False,
         temperature=0,
-        callbacks=[LoggingHandler(project_name, test_case)]
+        callbacks=[LoggingHandler(project_name, test_case)],
     )
     return llm, tools
 
@@ -120,8 +118,7 @@ async def on_submit(name_to_tool_map):
     )
     if st.session_state.ai_message_function_name:
         tool = name_to_tool_map[st.session_state.ai_message_function_name]
-        function_arguments = json.loads(
-            st.session_state.ai_message_function_arguments)
+        function_arguments = json.loads(st.session_state.ai_message_function_arguments)
         function_response = await tool._arun(
             **function_arguments,
         )
@@ -151,12 +148,11 @@ def save_conversation_history(project_name: str, test_case: str):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    with open(file_path, 'w') as f:
+    with open(file_path, "w") as f:
         f.write(json.dumps(st.session_state.messages, indent=4))
 
 
 async def main():
-
     # Initialize browser
     if st.session_state.browser is None:
         st.session_state.browser = await get_browser()
@@ -188,13 +184,16 @@ async def main():
         if message["role"] == "function":
             prompt_messages.append(FunctionMessage(**message))
             with st.chat_message("function"):
-                st.subheader(message["name"])
+                st.write(f"**{message['name']}**")
                 st.write(message["content"])
         elif message["role"] == "assistant":
             prompt_messages.append(AIMessage(**message))
-            with st.chat_message("system"):
-                st.write(message["content"])
-                function_call = message.get("function_call", None)
+            with st.chat_message("assistant"):
+                if message["content"]:
+                    st.write(message["content"])
+                function_call = message.get("additional_kwargs", {}).get(
+                    "function_call", None
+                )
                 if function_call:
                     with st.status(function_call["name"], state="complete"):
                         st.write(function_call["arguments"])
@@ -210,8 +209,8 @@ async def main():
 
     # User message
     if last_message == None or last_message["role"] == "assistant":
-        with st.form("user_message"):
-            st.form_submit_button(
+        if last_message is not None:
+            st.button(
                 "Save conversation history",
                 on_click=save_conversation_history,
                 args=(project_name, test_case),
@@ -219,13 +218,11 @@ async def main():
         with st.chat_message("user"):
             user_message_content = st.text_area(
                 "User message content",
-                "Let's write a test for a chat app. We should test a user can send a message.",
                 key="user_message_content",
                 label_visibility="collapsed",
             )
             if user_message_content:
-                prompt_messages.append(HumanMessage(
-                    content=user_message_content))
+                prompt_messages.append(HumanMessage(content=user_message_content))
             else:
                 st.stop()
 
@@ -262,20 +259,17 @@ async def main():
     with st.chat_message("assistant"):
         with st.form("ai_message"):
             st.text_area(
-                "AI message content",
+                "Content",
                 key="ai_message_content",
-                label_visibility="collapsed",
             )
             function_call = response.additional_kwargs.get("function_call", {})
             st.text_input(
-                "AI message function name",
+                "Function call",
                 key="ai_message_function_name",
-                label_visibility="collapsed",
             )
             st.text_area(
-                "AI message function arguments",
+                "Arguments",
                 key="ai_message_function_arguments",
-                label_visibility="collapsed",
             )
             st.form_submit_button(
                 "Commit agent completion",
