9081914f4b4acd9255841d6da51e79d86f769b86
manak <petr.manak@profiq.com>
2023-10-16 19:35:19 +0200

    removed code-generating from langchain module

--- langchain_modules/tools/playwright/fill_element.py
+++ langchain_modules/tools/playwright/fill_element.py
@@ -5,13 +5,11 @@ from typing import Type
 from pydantic import BaseModel, Field
 
 from langchain.tools.playwright.base import BaseBrowserTool
-from langchain.tools.playwright.utils import aget_current_page, get_current_page
-from .utils import awrite_to_file, awrite_fail_to_file
+from langchain.tools.playwright.utils import aget_current_page
 
 
 class FillToolInput(BaseModel):
     """Input for FillTool."""
-
     selector: str = Field(..., description="Selector for the element by text content.",)
     text: str = Field(..., description="Text what you want to fill up.")
 
@@ -26,42 +24,21 @@ class FillTextTool(BaseBrowserTool):
     # Timeout (in ms) for Playwright to wait for element to be ready.
     playwright_timeout: float = 2_000
 
-    def _run(
-        self,
-        selector: str,
-        text: str,
-    ) -> str:
-        """Use the tool."""
-        if self.sync_browser is None:
-            raise ValueError(f"Synchronous browser not provided to {self.name}")
-        page = get_current_page(self.sync_browser)
-        # try to enter the text on the element by text
-        try:
-            page.locator(selector).fill(text, timeout=self.playwright_timeout)
-             # write playwright command to temp file
-            playwright_cmd = f"    page.locator(\"{selector}\").fill('{text}');\n"
-            with open('tempfile', 'a') as f:
-                f.write(playwright_cmd)
-        except Exception as e:
-            return f"Unable to fill up text on element '{selector}' with exception: {e}"
+    @staticmethod
+    def _run() -> str:
+        """_run() isn't implemented, but is required to be defined."""
+        return "_run() not implemented."
 
-        return "Text input on the element by text", selector ,"was successfully performed"
-
-    async def _arun(
-        self,
-        selector: str,
-        text: str,
-    ) -> str:
+    async def _arun(self, selector: str, text: str) -> str:
         """Use the tool."""
         if self.async_browser is None:
             raise ValueError(f"Asynchronous browser not provided to {self.name}")
+
         page = await aget_current_page(self.async_browser)
-        playwright_cmd = f"await page.locator(\"{selector}\").fill('{text}');\n"
-        # try to enter the text on the element by text
+
         try:
             await page.locator(selector).fill(text, timeout=self.playwright_timeout)
-            await awrite_to_file(msg=f'    {playwright_cmd}')
-        except Exception as e:
-            await awrite_fail_to_file(msg=playwright_cmd, page=page)
-            return f"Unable to fill up text on element '{selector}' with exception: {e}"
+        except Exception:
+            return f"Unable to fill up text on element '{selector}'."
+
         return f"Text input on the element by text, {selector} ,was successfully performed"
