19ad0012e8832f2d62e5db7063834914acba746d
manak <petr.manak@profiq.com>
2023-10-26 16:07:16 +0200

    pre-auto-fill

---
+++ function_caller.py
@@ -0,0 +1,35 @@
+import asyncio
+import json
+
+from dotenv import load_dotenv
+from playwright.async_api import async_playwright
+
+from langchain_modules.toolkit import PlayWrightBrowserToolkit
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+load_dotenv()
+
+
+async def get_browser():
+    browser = await async_playwright().start()
+    async_browser = await browser.chromium.launch(headless=False)
+    return async_browser
+
+
+async def get_tools(browser) -> list:
+    async_browser = browser
+    toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
+    tools = toolkit.get_tools()
+    return tools
+
+
+async def call_function(browser, json_function: json):
+    tools = await get_tools(browser)
+    name_to_tool_map = {tool.name: tool for tool in tools}
+
+    tool = name_to_tool_map[json_function["name"]]
+    function_arguments = json.loads(json_function["arguments"])
+    function_response = await tool._arun(**function_arguments)
+    return function_response
