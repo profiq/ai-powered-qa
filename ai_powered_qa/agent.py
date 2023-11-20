ba388f8a092f04f15cb3d3f49a2593992fe7a13e
Milos Svana <milos.svana@gmail.com>
2023-11-20 12:04:35 +0100

    Ass support for arguments when calling a tool

--- ai_powered_qa/agent.py
+++ ai_powered_qa/agent.py
@@ -2,9 +2,13 @@ import openai
 import docstring_parser
 import inspect
 import random
+import json
+
 
 class Agent:
-    def __init__(self, system_message="You are a helpful assistant.", model="gpt-3.5-turbo-1106"):
+    def __init__(
+        self, system_message="You are a helpful assistant.", model="gpt-3.5-turbo-1106"
+    ):
         self._system_message = system_message
         self._model = model
         self._client = openai.OpenAI()
@@ -15,15 +19,14 @@ class Agent:
 
     def get_completion(self, user_prompt: str, model=None):
         model = model or self._model
-        functions = self._get_function_list()
         completion = self._client.chat.completions.create(
             messages=[
                 {"role": "system", "content": self._system_message},
                 *self._conversation_history,
-                {"role": "user", "content": user_prompt}
+                {"role": "user", "content": user_prompt},
             ],
             model=model,
-            tools=functions
+            tools=self._tools_for_gpt,
         )
 
         self._request_history.append(completion)
@@ -32,23 +35,26 @@ class Agent:
     def add_plugin(self, plugin):
         self._plugins.append(plugin)
         for t in plugin.tools:
-            self._tools[t[0]['name']] = (t[1], plugin)
+            self._tools[t[0]["name"]] = t
 
-    def append_message(self, role: str, content: str, tool_calls: None|list = None):
+    def append_message(self, role: str, content: str, tool_calls: None | list = None):
         print(role, content, tool_calls)
 
         if not tool_calls:
-            self._conversation_history.append({'role': role, 'content': content})
+            self._conversation_history.append({"role": role, "content": content})
             return
 
-        self._conversation_history.append({'role': 'assistant', 'content': content, 'tool_calls': tool_calls})
+        self._conversation_history.append(
+            {"role": "assistant", "content": content, "tool_calls": tool_calls}
+        )
 
         for tool_call in tool_calls:
             print(tool_call)
             tool = self._tools[tool_call.function.name]
-            result = tool[0]()
-            self._conversation_history.append({'role': 'tool', 'content': str(result), 'tool_call_id': tool_call.id})
-
+            result = tool[1](**json.loads(tool_call.function.arguments))
+            self._conversation_history.append(
+                {"role": "tool", "content": str(result), "tool_call_id": tool_call.id}
+            )
 
         completion = self._client.chat.completions.create(
             messages=[
@@ -56,20 +62,16 @@ class Agent:
                 *self._conversation_history,
             ],
             model=self._model,
-            tools=self._get_function_list()
+            tools=self._tools_for_gpt,
         )
 
         message = completion.choices[0].message
-        self.append_message('assistant', message.content, message.tool_calls)
-            
+        self.append_message("assistant", message.content, message.tool_calls)
 
-    def _get_function_list(self):
-        functions = []
-        for plugin in self._plugins:
-            for t in plugin.tools:
-                functions.append({"type": "function", "function": t[0]})
-        print(functions)
-        return functions
+    @property
+    def _tools_for_gpt(self) -> list[dict]:
+        tools = [{"type": "function", "function": t[0]} for t in self._tools.values()]
+        return tools
 
 
 def tool(method):
@@ -79,7 +81,7 @@ def tool(method):
 
 class Plugin:
     TYPE_MAP = {
-        'int': 'integer',
+        "int": "integer",
     }
 
     def __init__(self):
@@ -92,16 +94,16 @@ class Plugin:
 
     def _register_tools(self):
         for name, method in inspect.getmembers(self, predicate=inspect.ismethod):
-            if hasattr(method, '__tool__'):
+            if hasattr(method, "__tool__"):
                 docstring = inspect.getdoc(method)
                 docstring_parsed = docstring_parser.parse(docstring)
                 tool_description = {
-                    'name': name,
-                    'description': docstring_parsed.short_description,
-                    'parameters': {
-                        'type': 'object',
-                        'properties': self._build_param_object(docstring_parsed.params)
-                    }
+                    "name": f"{self.__class__.__name__}_{name}",
+                    "description": docstring_parsed.short_description,
+                    "parameters": {
+                        "type": "object",
+                        "properties": self._build_param_object(docstring_parsed.params),
+                    },
                 }
                 self._tools.append((tool_description, method))
 
@@ -109,13 +111,13 @@ class Plugin:
         param_object = {}
         for param in params:
             param_object[param.arg_name] = {
-                'type': self.TYPE_MAP.get(param.type_name, param.type_name),
-                'description': param.description,
+                "type": self.TYPE_MAP.get(param.type_name, param.type_name),
+                "description": param.description,
             }
         return param_object
 
-class RandomNumberPlugin(Plugin):
 
+class RandomNumberPlugin(Plugin):
     @tool
     def get_random_number(self, min_number: int = 0, max_number: int = 100):
         """
@@ -127,10 +129,10 @@ class RandomNumberPlugin(Plugin):
         return random.randint(min_number, max_number)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     agent = Agent()
     agent.add_plugin(RandomNumberPlugin())
 
     message = agent.get_completion("Get me a random number betweent 1 and 30.")
-    agent.append_message('user', "Get me a random number between 1 and 30.")
+    agent.append_message("user", "Get me a random number between 1 and 30.")
     agent.append_message(message.role, message.content, message.tool_calls)
