f4a37e9d89604a3f3401786887cc5e37bc2345b7
Milos Svana <milos.svana@gmail.com>
2023-12-19 13:23:11 +0100

    Add support for context messages to the Agent class and write a simple test for the todo_plugin

--- ai_powered_qa/custom_plugins/todo_plugin.py
+++ ai_powered_qa/custom_plugins/todo_plugin.py
@@ -1,4 +1,5 @@
-from ai_powered_qa.components import Plugin, tool
+from ai_powered_qa.components.plugin import Plugin, tool
+from ai_powered_qa.components.agent import Agent
 
 
 class TodoPlugin(Plugin):
@@ -13,6 +14,7 @@ class TodoPlugin(Plugin):
         ctx = "LIST OF TODOS:\n"
         for todo in self.todos:
             ctx += f"[{'COMPLETED' if todo['completed'] else 'TODO'}] {todo['title']}\n"
+        return ctx
 
     @tool
     def add_todo(self, title: str):
@@ -49,3 +51,34 @@ class TodoPlugin(Plugin):
                 self.todos.remove(todo)
                 return f"Removed todo: {title}"
         return f"Could not find todo: {title}"
+
+
+if __name__ == "__main__":
+    todo_plugin = TodoPlugin()
+
+    agent = Agent(agent_name="TodoAgent", model="gpt-4-1106-preview")
+    agent.add_plugin(todo_plugin)
+    interaction = agent.generate_interaction("Add grocery shopping to my todo list.")
+    agent.commit_interaction(interaction)
+
+    interaction = agent.generate_interaction("Add working on AI powered QA to my todo list.")
+    agent.commit_interaction(interaction)
+
+    print("Todos after adding items:")
+    print(todo_plugin.context_message)
+
+    interaction = agent.generate_interaction("Mark grocery shopping as completed.")
+    agent.commit_interaction(interaction)
+
+    print("Todos after marking grocery shopping as completed:")
+    print(todo_plugin.context_message)
+
+    interaction = agent.generate_interaction("Do I still have any incomplete items on my todo list?")
+    print(interaction.agent_response)
+
+    interaction = agent.generate_interaction("Remove all items from my todo list.")
+    print(interaction)
+    agent.commit_interaction(interaction)
+
+    print("Todos after removing all completed items:")
+    print(todo_plugin.context_message)
