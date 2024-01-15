aa30daf069049790cb005054f189c5fdff315b94
bednarp <petr.bednar@profiq.com>
2024-01-15 14:09:32 +0100

    Add assert tool

--- ai_powered_qa/custom_plugins/playwright_plugin.py
+++ ai_powered_qa/custom_plugins/playwright_plugin.py
@@ -122,6 +122,44 @@ class PlaywrightPlugin(Plugin):
             return f"Unable to fill up text on element '{selector}'."
         return f"Text input on the element by text, {selector}, was successfully performed."
 
+    @tool
+    def assert_that(self, selector: str, action: str, value: str = None):
+        """
+        Perform an assertion on an element based on its text content.
+
+        :param str selector: Selector for the element identified by its text content.
+        :param str action: [
+                  {
+                    "option": "is_visible",
+                    "description": "Check if the element is displayed in the content.",
+                  },
+                  {
+                    "option": "contain_text",
+                    "description": "Check if the element contains the specified text content.",
+                  }
+                ]
+        :param str value: Text to comparing in action contain_text or None.
+        """
+        return self.run_async(self._assert_that(selector, action, value))
+
+    async def _assert_that(self, selector: str, action: str, value: str = None):
+        page = await self.ensure_page()
+
+        if action == "is_visible":
+            state = await page.locator(selector).is_visible()
+            if state:
+                return f"{selector} is visible in context, action passed."
+            return f"{selector} is not visible in context, action passed."
+        if action == "contain_text":
+            text = await page.inner_text(selector)
+            if text == "":
+                text = await page.locator(selector).get_attribute("value")
+            if value == text:
+                return f"{selector} contain '{value}', action passed."
+            return f"{selector} contain '{text}' not '{value}', action passed."
+        return "Not implemented action"
+
+
     async def ensure_page(self) -> playwright.async_api.Page:
         if not self._page:
             self._playwright = await playwright.async_api.async_playwright().start()
